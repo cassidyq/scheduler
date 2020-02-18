47fd036f61b9a427a7cfe48c3d64df0079ffc7e4
cassidyq <cassidy@referralsaasquatch.com>
2020-02-18 14:39:46 -0800

    Added axios as a dependency so the app can fetch data from the scheduler-api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import 'components/Appointment';
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -80,7 +64,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then((response) => {
+        setDays(response.data);
+      })
+  }, []);
 
   return (
     <main className="layout">
